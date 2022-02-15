f0fa09958c28e41fceb84a34b2966bdadd5b61b7
Pedro Braga <pedrobragaom@gmail.com>
2022-02-15 11:13:47 -0300

    Initial commit

---
+++ src/test/java/br/com/pucminas/elderest/ElderestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pucminas.elderest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElderestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
