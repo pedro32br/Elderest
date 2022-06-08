90af73c0f3f050a99fd080182735a656c36fe0a5
Pedro Braga <pedro.braga@ppro.com>
2022-06-07 23:00:02 -0300

    Included Spring security and Loginform.js

--- web/elderest-web/src/App.js
+++ web/elderest-web/src/App.js
@@ -1,5 +1,42 @@
+import React, { useState } from "react";
+
+import LoginForm from "./components/LoginForm";
+
 function App() {
-  return <div>Hello!!</div>;
+  const adminUser = {
+    email: "admin@admin.com",
+    passward: "admin123",
+  };
+
+  const [user, setUser] = useState({ name: "", email: "" });
+  const [error, setError] = useState("");
+
+  const Login = (details) => {
+    console.log(details);
+
+    if (details.email == adminUser.email && details.password == adminUser.password) {
+      console.log("You are log in");
+    }
+  };
+
+  const Logout = () => {
+    console.log("Logout");
+  };
+
+  return (
+    <div className="App">
+      {user.email != "" ? (
+        <div className="welcome">
+          <h2>
+            Welcome, <span>{user.name}</span>
+          </h2>
+          <button>Logout</button>
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error} />
+      )}
+    </div>
+  );
 }
 
 export default App;
